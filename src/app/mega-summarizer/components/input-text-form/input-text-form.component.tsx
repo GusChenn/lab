c45c4a14ba4c373b9fbacb3b6dc0a7a1a39b7e15
Gustavo Chen <gustavo.sao@gmail.com>
2024-06-08 15:59:28 -0300

    feat(input-form): redesign form and add Font Awesome icons
    The form has been redesigned to be more user-friendly and intuitive.
    The textarea has been replaced with an input field. Two buttons with
    Font Awesome icons have been added: one for voice input and one for
    submitting the text. The Font Awesome library has been added to the
    project dependencies and imported into the global styles.

--- src/app/mega-summarizer/components/input-text-form/input-text-form.component.tsx
+++ src/app/mega-summarizer/components/input-text-form/input-text-form.component.tsx
@@ -7,22 +7,35 @@ export default function InputTextForm() {
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    const text = event.currentTarget.querySelector("textarea")?.value || "";
+    const text = event.currentTarget.querySelector("input")?.value || "";
 
     const summary = await fetchSummary(text);
     fillResultContent(summary);
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit} className="mb-6">
-        <textarea
-          className="textarea mb-4"
+    <div className="box box-radius">
+      <form
+        onSubmit={handleSubmit}
+        className="is-flex is-flex-wrap-nowrap is-align-items-center"
+      >
+        <input
+          type="text"
+          className="input is-flex-grow-3 mr-3"
           placeholder="Write your story here..."
           aria-label="Write your story here"
-        ></textarea>
-        <button className="button is-primary" type="submit">
-          Summarize
+        />
+        <button
+          className="button is-primary is-rounded is-flex-grow-1 mr-3"
+          type="button"
+        >
+          <i className="fas fa-microphone"></i>
+        </button>
+        <button
+          className="button is-primary is-rounded is-flex-grow-1"
+          type="submit"
+        >
+          <i className="fas fa-arrow-right"></i>
         </button>
       </form>
     </div>
